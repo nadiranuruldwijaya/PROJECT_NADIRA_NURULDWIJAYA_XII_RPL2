da0e7faba8e07cef6ef5f653a23c2715281b0c4d
nadiranuruldwijaya <nadiradwijaya26@gmail.com>
2020-09-18 15:09:06 +0700

    [W]LOGIN MULTIPLE AUTH

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AdminController extends Controller
+{
+   Public Function awal()
+   {
+   	return "Ini Admin Hey";
+   }
+}
